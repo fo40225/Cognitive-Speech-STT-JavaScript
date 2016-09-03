79a41c101f2ae7d07582cc2013b98599c9a73eee
fo40225 <fo40225@hotmail.com>
2016-09-03 21:30:01 +0800

    fix Bing._cu is null on FF33.
    root cause is createSpeech() return null after Bing._cu has been assignmented.

--- speech.1.0.0.js
+++ speech.1.0.0.js
@@ -13,8 +13,8 @@
 // 
 // LICENSE
 // -------
-// © 2015 Microsoft. All rights reserved.  
-// This document is provided ìas-isî. Information and views expressed in this document, including URL and other Internet Web site references, may change without notice.  
+// ¬© 2015 Microsoft. All rights reserved.  
+// This document is provided ‚Äúas-is‚Äù. Information and views expressed in this document, including URL and other Internet Web site references, may change without notice.  
 // Some examples depicted herein are provided for illustration only and are fictitious.  No real association or connection is intended or should be inferred. 
 // This document does not provide you with any legal rights to any intellectual property in any Microsoft product. You may copy and use this document for your internal, reference purposes. This 
 // document is confidential and proprietary to Microsoft. It is disclosed and can be used only pursuant to a non-disclosure agreement. 
@@ -652,7 +652,10 @@ var Bing;
     function initialize() {
         var i;
         Bing._window.useStringArrays = false;
-        Bing._cu = createSpeech();
+        var s =  createSpeech();
+        if (s) {
+            Bing._cu = s;
+        }
         if (!Bing._cuDeferred) {
             Bing._cuDeferred = [];
         }
